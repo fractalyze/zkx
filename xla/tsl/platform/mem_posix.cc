00fac446ff076409bde5878f4ac0945a736066c3
Ryan Kim <chokobole33@gmail.com>
2025-02-28 15:51:50 +0900

    feat(tsl): add memory (de)allocation functions

---
+++ xla/tsl/platform/mem_posix.cc
@@ -0,0 +1,56 @@
+/* Copyright 2015 The TensorFlow Authors. All Rights Reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+==============================================================================*/
+
+#include <stdlib.h>
+
+#include "xla/tsl/platform/mem.h"
+
+namespace tsl::port {
+
+void* AlignedMalloc(size_t size, int minimum_alignment) {
+#if defined(__ANDROID__)
+  return memalign(minimum_alignment, size);
+#else  // !defined(__ANDROID__)
+  void* ptr = nullptr;
+  // posix_memalign requires that the requested alignment be at least
+  // sizeof(void*). In this case, fall back on malloc which should return
+  // memory aligned to at least the size of a pointer.
+  const int required_alignment = sizeof(void*);
+  if (minimum_alignment < required_alignment) return Malloc(size);
+  int err = posix_memalign(&ptr, minimum_alignment, size);
+  if (err != 0) {
+    return nullptr;
+  } else {
+    return ptr;
+  }
+#endif
+}
+
+void AlignedFree(void* aligned_memory) { Free(aligned_memory); }
+
+void AlignedSizedFree(void* aligned_memory, size_t alignment, size_t size) {
+  (void)alignment;
+  (void)size;
+
+  Free(aligned_memory);
+}
+
+void* Malloc(size_t size) { return malloc(size); }
+
+void* Realloc(void* ptr, size_t size) { return realloc(ptr, size); }
+
+void Free(void* ptr) { free(ptr); }
+
+}  // namespace tsl::port
