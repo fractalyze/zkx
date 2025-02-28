c246646a37f3f8cbc18c8adde3cf50b92ca4fdbe
Ryan Kim <chokobole33@gmail.com>
2025-02-28 14:29:14 +0900

    feat(base): add `BoolToString()`

--- zkx/base/strings/string_util.h
+++ zkx/base/strings/string_util.h
@@ -7,6 +7,8 @@ namespace zkx::base {
 
 std::string HumanReadableElapsedTime(double seconds);
 
+std::string_view BoolToString(bool b);
+
 }  // namespace zkx::base
 
 #endif  // ZKX_BASE_STRINGS_STRING_UTIL_H_
