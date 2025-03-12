2598b19989e7155e98bf14c8d0065c3e0cbf6ffa
Ryan Kim <chokobole33@gmail.com>
2025-03-12 17:41:13 +0900

    feat(core): add `Clique`

---
+++ zkx/core/collectives/clique.cc
@@ -0,0 +1,44 @@
+/* Copyright 2024 The OpenXLA Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+==============================================================================*/
+
+#include "zkx/core/collectives/clique.h"
+
+namespace zkx {
+
+std::optional<Communicator*> Clique::comm(RankId rank) const {
+  if (auto it = communicators_.find(rank); it != communicators_.end()) {
+    return it->second.get();
+  }
+  return std::nullopt;
+}
+
+void Clique::ForEachComm(
+    absl::FunctionRef<void(RankId, Communicator*)> fn) const {
+  for (auto& [rank, comm] : communicators_) {
+    fn(rank, comm.get());
+  }
+}
+
+absl::Status Clique::AddComm(RankId rank,
+                             std::unique_ptr<Communicator> communicator) {
+  auto emplaced = communicators_.emplace(rank, std::move(communicator));
+  if (!emplaced.second) {
+    return absl::InvalidArgumentError(
+        absl::StrFormat("Rank %d already exists in clique", rank.value()));
+  }
+  return absl::OkStatus();
+}
+
+}  // namespace zkx
