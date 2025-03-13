3f8e204f8929ea3a81f7ec313dd669bac709156b
Ryan Kim <chokobole33@gmail.com>
2025-03-13 13:11:43 +0900

    feat(backends/cpu): add `CpuCollectives`

---
+++ zkx/backends/cpu/collectives/cpu_collectives.cc
@@ -0,0 +1,55 @@
+/* Copyright 2024 The OpenXLA Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+==============================================================================*/
+
+#include "zkx/backends/cpu/collectives/cpu_collectives.h"
+
+#include "absl/log/check.h"
+#include "absl/log/log.h"
+
+#include "xla/tsl/platform/casts.h"
+#include "zkx/core/collectives/collectives_registry.h"
+
+namespace zkx::cpu {
+
+CpuCollectives* CpuCollectives::Default() {
+  absl::StatusOr<Collectives*> collectives =
+      CollectivesRegistry::Default("host");
+  CHECK_OK(collectives) << "Failed to get CPU collectives";  // Crash OK
+
+  if (auto* cpu_collectives = tsl::down_cast<CpuCollectives*>(*collectives)) {
+    return cpu_collectives;
+  }
+
+  LOG(FATAL) << "Unsupported collectives implementation for CPU";
+}
+
+absl::StatusOr<const CpuCollectives::Device*> CpuCollectives::TryCast(
+    const Collectives::Device* device) {
+  if (auto* cpu_device = tsl::down_cast<const Device*>(device)) {
+    return cpu_device;
+  }
+  return absl::InvalidArgumentError("Collectives device is not a CPU device");
+}
+
+absl::StatusOr<const CpuCollectives::Executor*> CpuCollectives::TryCast(
+    const Communicator::Executor* executor) {
+  if (auto* cpu_executor = tsl::down_cast<const Executor*>(executor)) {
+    return cpu_executor;
+  }
+  return absl::InvalidArgumentError(
+      "Collectives executor is not a CPU executor");
+}
+
+}  // namespace zkx::cpu
---
+++ zkx/backends/cpu/collectives/cpu_collectives.h
@@ -0,0 +1,77 @@
+/* Copyright 2024 The OpenZKX Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+==============================================================================*/
+
+#ifndef ZKX_BACKENDS_CPU_COLLECTIVES_CPU_COLLECTIVES_H_
+#define ZKX_BACKENDS_CPU_COLLECTIVES_CPU_COLLECTIVES_H_
+
+#include "absl/status/status.h"
+#include "absl/status/statusor.h"
+#include "absl/time/time.h"
+
+#include "zkx/core/collectives/collectives.h"
+#include "zkx/service/collective_ops_utils.h"
+
+namespace zkx::cpu {
+
+// ZKX:CPU extension of the Collectives interface with CPU-specific APIs.
+class CpuCollectives : public Collectives {
+ public:
+  // Returns the default collectives implementation for CPU backend.
+  static CpuCollectives* Default();
+
+  class Device : public Collectives::Device {
+   public:
+    Device() = default;
+  };
+
+  // Executor allows CPU collectives clients to pass additional information to
+  // the collectives implementation.
+  class Executor : public Communicator::Executor {
+   public:
+    Executor(RendezvousKey rendezvous_key, absl::Duration timeout)
+    : rendezvous_key_(rendezvous_key), timeout_(timeout) {}
+
+    const RendezvousKey& rendezvous_key() const { return rendezvous_key_; }
+    const absl::Duration& timeout() const { return timeout_; }
+
+   private:
+    RendezvousKey rendezvous_key_;
+    absl::Duration timeout_;
+  };
+
+  absl::StatusOr<CliqueId> CreateUniqueCliqueId() const final {
+    return absl::UnimplementedError(
+        "CPU collectives do not support clique ids");
+  }
+
+  absl::StatusOr<std::vector<std::unique_ptr<Communicator>>> SplitCommunicators(
+      absl::Span<const Communicator* const> comms, int32_t color,
+      absl::Span<const RankId> keys, const Config& config) final {
+    return absl::UnimplementedError(
+        "CPU collectives do not support communicator splitting");
+  }
+
+  // Tries to cast a Collectives::Device to a CpuCollectives::Device.
+  static absl::StatusOr<const Device*> TryCast(
+      const Collectives::Device* device);
+
+  // Tries to cast a Communicator::Executor to a CpuCollectives::Executor.
+  static absl::StatusOr<const Executor*> TryCast(
+      const Communicator::Executor* executor);
+};
+
+}  // namespace zkx::cpu
+
+#endif  // ZKX_BACKENDS_CPU_COLLECTIVES_CPU_COLLECTIVES_H_
