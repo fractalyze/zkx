fe43216f403a16c1e714183a4e97a7eb46fbd6bd
Ryan Kim <chokobole33@gmail.com>
2025-04-04 14:32:12 +0900

    feat(tsl): add `AppendToMessage()`

--- xla/tsl/platform/errors.h
+++ xla/tsl/platform/errors.h
@@ -16,8 +16,53 @@ limitations under the License.
 #ifndef XLA_TSL_PLATFORM_ERRORS_H_
 #define XLA_TSL_PLATFORM_ERRORS_H_
 
+#include <string>
+#include <unordered_map>
+#include <utility>
+
 #include "absl/base/optimization.h"
 #include "absl/status/status.h"
+#include "absl/strings/str_cat.h"
+
+namespace tsl::errors {
+namespace internal {
+
+// Returns all payloads from a Status as a key-value map.
+std::unordered_map<std::string, std::string> GetPayloads(
+    const absl::Status& status);
+
+// Inserts all given payloads into the given status. Will overwrite existing
+// payloads if they exist with the same key.
+void InsertPayloads(
+    absl::Status& status,
+    const std::unordered_map<std::string, std::string>& payloads);
+
+// Copies all payloads from one Status to another. Will overwrite existing
+// payloads in the destination if they exist with the same key.
+void CopyPayloads(const absl::Status& from, absl::Status& to);
+
+absl::Status Create(
+    absl::StatusCode code, std::string_view message,
+    const std::unordered_map<std::string, std::string>& payloads);
+
+// Returns a new Status, replacing its message with the given.
+absl::Status CreateWithUpdatedMessage(const absl::Status& status,
+                                      std::string_view message);
+
+}  // namespace internal
+
+// Append some context to an error message.  Each time we append
+// context put it on a new line, since it is possible for there
+// to be several layers of additional context.
+template <typename... Args>
+void AppendToMessage(absl::Status* status, Args... args) {
+  auto new_status = internal::CreateWithUpdatedMessage(
+      *status, absl::StrCat(status->message(), "\n\t", args...));
+  internal::CopyPayloads(*status, new_status);
+  *status = std::move(new_status);
+}
+
+}  // namespace tsl::errors
 
 // For propagating errors when calling a function.
 #define TF_RETURN_IF_ERROR(...)              \
