2c386a70395d9c64830c538f6109018a7e71826a
Ryan Kim <chokobole33@gmail.com>
2025-04-28 16:38:35 +0900

    feat(service): enable more methods on `PlatformUtil`

--- zkx/service/platform_util.cc
+++ zkx/service/platform_util.cc
@@ -16,8 +16,19 @@ limitations under the License.
 #include "zkx/service/platform_util.h"
 
 #include "absl/strings/ascii.h"
+#include "absl/strings/str_join.h"
+
+#include "xla/tsl/platform/errors.h"
+#include "xla/tsl/platform/statusor.h"
+#include "zkx/base/logging.h"
+#include "zkx/service/compiler.h"
+#include "zkx/stream_executor/platform_manager.h"
 
 namespace zkx {
+
+// The name of the interpreter platform.
+constexpr char kInterpreter[] = "interpreter";
+
 namespace {
 
 std::string CanonicalPlatformName(std::string_view platform_name) {
@@ -41,11 +52,72 @@ std::string CanonicalPlatformName(std::string_view platform_name) {
   return lowercase_platform_name;
 }
 
+absl::StatusOr<std::vector<se::Platform*>> GetSupportedPlatforms() {
+  return se::PlatformManager::PlatformsWithFilter(
+      [](const se::Platform* platform) {
+        auto compiler_status = Compiler::GetForPlatform(platform);
+        bool supported = compiler_status.ok();
+        if (!supported) {
+          LOG(INFO) << "platform " << platform->Name() << " present but no "
+                    << "ZKX compiler available: "
+                    << compiler_status.status().message();
+        }
+        return supported;
+      });
+}
+
 }  // namespace
 
 absl::StatusOr<std::string> PlatformUtil::CanonicalPlatformName(
     std::string_view platform_name) {
   return zkx::CanonicalPlatformName(platform_name);
+}
+
+absl::StatusOr<std::vector<se::Platform*>>
+PlatformUtil::GetSupportedPlatforms() {
+  // Gather all platforms which have an XLA compiler.
+  return zkx::GetSupportedPlatforms();
+}
+
+absl::StatusOr<se::Platform*> PlatformUtil::GetDefaultPlatform() {
+  TF_ASSIGN_OR_RETURN(auto platforms, GetSupportedPlatforms());
+
+  se::Platform* platform = nullptr;
+  if (platforms.empty()) {
+    return absl::NotFoundError("no platforms found");
+  } else if (platforms.size() == 1) {
+    platform = platforms[0];
+  } else if (platforms.size() == 2) {
+    for (int i = 0; i < 2; i++) {
+      if (absl::AsciiStrToLower(platforms[i]->Name()) == kInterpreter &&
+          absl::AsciiStrToLower(platforms[1 - i]->Name()) != kInterpreter) {
+        platform = platforms[1 - i];
+        break;
+      }
+    }
+  }
+  if (platform != nullptr) {
+    return platform;
+  }
+
+  // Multiple platforms present and we can't pick a reasonable default.
+  std::string platforms_string = absl::StrJoin(
+      platforms, ", ",
+      [](std::string* out, const se::Platform* p) { out->append(p->Name()); });
+  return absl::InvalidArgumentError(absl::StrFormat(
+      "must specify platform because more than one platform (except for the "
+      "interpreter platform) found: %s.",
+      platforms_string));
+}
+
+// static
+absl::StatusOr<se::Platform*> PlatformUtil::GetPlatform(
+    std::string_view platform_name) {
+  TF_ASSIGN_OR_RETURN(se::Platform * platform,
+                      se::PlatformManager::PlatformWithName(
+                          zkx::CanonicalPlatformName(platform_name)));
+  TF_RETURN_IF_ERROR(Compiler::GetForPlatform(platform).status());
+  return platform;
 }
 
 }  // namespace zkx
