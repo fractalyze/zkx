3b606aa630372cfeb13f4fbb576d61d14b85adf0
Ryan Kim <chokobole33@gmail.com>
2025-05-03 22:10:28 +0900

    feat(tsl): add memory utilities

--- xla/tsl/platform/mem.h
+++ xla/tsl/platform/mem.h
@@ -17,6 +17,7 @@ limitations under the License.
 #define XLA_TSL_PLATFORM_MEM_H_
 
 #include <stddef.h>
+#include <stdint.h>
 
 namespace tsl::port {
 
@@ -44,6 +45,53 @@ struct AlignedAllocator {
 void* Malloc(size_t size);
 void* Realloc(void* ptr, size_t size);
 void Free(void* ptr);
+
+// Tries to release num_bytes of free memory back to the operating
+// system for reuse.  Use this routine with caution -- to get this
+// memory back may require faulting pages back in by the OS, and
+// that may be slow.
+//
+// Currently, if a malloc implementation does not support this
+// routine, this routine is a no-op.
+void MallocExtension_ReleaseToSystem(size_t num_bytes);
+
+// Returns the actual number N of bytes reserved by the malloc for the
+// pointer p.  This number may be equal to or greater than the number
+// of bytes requested when p was allocated.
+//
+// This routine is just useful for statistics collection.  The
+// client must *not* read or write from the extra bytes that are
+// indicated by this call.
+//
+// Example, suppose the client gets memory by calling
+//    p = malloc(10)
+// and GetAllocatedSize(p) may return 16.  The client must only use the
+// first 10 bytes p[0..9], and not attempt to read or write p[10..15].
+//
+// Currently, if a malloc implementation does not support this
+// routine, this routine returns 0.
+size_t MallocExtension_GetAllocatedSize(const void* p);
+
+struct MemoryInfo {
+  int64_t total = 0;
+  int64_t free = 0;
+};
+
+struct MemoryBandwidthInfo {
+  int64_t bw_used = 0;  // memory bandwidth used across all CPU (in MBs/second)
+};
+
+// Retrieves the host memory information. If any of the fields in the returned
+// MemoryInfo structure is INT64_MAX, it means such information is not
+// available.
+MemoryInfo GetMemoryInfo();
+
+// Retrieves the host memory bandwidth information. If any field in the returned
+// structure is INT64_MAX, it means such information is not available.
+MemoryBandwidthInfo GetMemoryBandwidthInfo();
+
+// Returns the amount of RAM available in bytes, or INT64_MAX if unknown.
+inline int64_t AvailableRam() { return GetMemoryInfo().free; }
 
 }  // namespace tsl::port
 
