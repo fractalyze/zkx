d77a7dda7bb3145e4030c416ce9136052b6791c4
Ryan Kim <chokobole33@gmail.com>
2025-05-05 14:45:33 +0900

    feat(tsl): add `CallOptions`

---
+++ xla/tsl/distributed_runtime/call_options.cc
@@ -0,0 +1,54 @@
+/* Copyright 2016 The TensorFlow Authors. All Rights Reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+==============================================================================*/
+
+#include "xla/tsl/distributed_runtime/call_options.h"
+
+#include <utility>
+
+namespace tsl {
+
+CallOptions::CallOptions() = default;
+
+void CallOptions::StartCancel() {
+  absl::MutexLock l(&mu_);
+  if (cancel_func_ != nullptr) {
+    // NOTE: We must call the `cancel_func_` with mu_ held. This ensure
+    // that ClearCancelCallback() does not race with StartCancel().
+    cancel_func_();
+    // NOTE: We can clear `cancel_func_` if needed.
+  }
+}
+
+void CallOptions::SetCancelCallback(CancelFunction cancel_func) {
+  absl::MutexLock l(&mu_);
+  cancel_func_ = std::move(cancel_func);
+}
+
+void CallOptions::ClearCancelCallback() {
+  absl::MutexLock l(&mu_);
+  cancel_func_ = nullptr;
+}
+
+absl::Duration CallOptions::GetTimeout() {
+  absl::MutexLock l(&mu_);
+  return timeout_;
+}
+
+void CallOptions::SetTimeout(absl::Duration timeout) {
+  absl::MutexLock l(&mu_);
+  timeout_ = timeout;
+}
+
+}  // end namespace tsl
