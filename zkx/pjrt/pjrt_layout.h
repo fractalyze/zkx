db75435743f6d3e27d2a50ab3bac927e3a4f52a1
Ryan Kim <chokobole33@gmail.com>
2025-05-08 07:10:35 +0900

    feat(pjrt): add `PjrtLayout`

---
+++ zkx/pjrt/pjrt_layout.h
@@ -0,0 +1,73 @@
+/* Copyright 2024 The TensorFlow Authors. All Rights Reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+==============================================================================*/
+
+#ifndef ZKX_PJRT_PJRT_LAYOUT_H_
+#define ZKX_PJRT_PJRT_LAYOUT_H_
+
+#include <memory>
+#include <string>
+#include <utility>
+
+#include "absl/hash/hash.h"
+#include "absl/status/statusor.h"
+
+#include "xla/tsl/platform/statusor.h"
+#include "zkx/hlo/parser/hlo_parser.h"
+#include "zkx/layout.h"
+
+namespace zkx {
+
+// Represents the memory layout of a PjRtBuffer.
+class PjRtLayout {
+ public:
+  explicit PjRtLayout(Layout layout) : layout_(std::move(layout)) {
+    // Strip memory space and set it to the default. PJRT tracks memory space
+    // separately from layout.
+    layout_.set_memory_space(Layout::kDefaultMemorySpace);
+  }
+
+  PjRtLayout(PjRtLayout& other) = delete;
+  PjRtLayout& operator=(const PjRtLayout& other) = delete;
+
+  static absl::StatusOr<std::shared_ptr<const PjRtLayout>> Deserialize(
+      std::string_view serialized) {
+    TF_ASSIGN_OR_RETURN(Layout layout, ParseLayout(serialized));
+    return std::make_shared<PjRtLayout>(std::move(layout));
+  }
+
+  const Layout& layout() const { return layout_; }
+
+  // Returns the serialized layout as a string.
+  std::string Serialize() const { return layout_.ToString(); }
+
+  // Human-readable string for error messages, user introspection, etc.
+  std::string ToString() const { return layout_.ToString(); }
+
+  bool operator==(const PjRtLayout& other) const {
+    return layout_ == other.layout_;
+  }
+
+  template <typename H>
+  friend H AbslHashValue(H state, const PjRtLayout& layout) {
+    return H::combine(std::move(state), layout.layout_);
+  }
+
+ private:
+  Layout layout_;
+};
+
+}  // namespace zkx
+
+#endif  // ZKX_PJRT_PJRT_LAYOUT_H_
