95e64b8ef842d2ade5f4919c256e45118ab95155
Ryan Kim <chokobole33@gmail.com>
2025-05-19 10:47:44 +0900

    feat: add `BigInt`

---
+++ zkx/math/base/bit_traits_forward.h
@@ -0,0 +1,11 @@
+#ifndef ZKX_MATH_BASE_BIT_TRAITS_FORWARD_H_
+#define ZKX_MATH_BASE_BIT_TRAITS_FORWARD_H_
+
+namespace zkx::math {
+
+template <typename T, typename SFINAE = void>
+class BitTraits;
+
+}  // namespace zkx::math
+
+#endif  // ZKX_MATH_BASE_BIT_TRAITS_FORWARD_H_
