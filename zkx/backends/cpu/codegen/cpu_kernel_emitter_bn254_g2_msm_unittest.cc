9edde9cbc23cffe75b0f3598d5e1a14e754aa330
Ryan Kim <chokobole33@gmail.com>
2025-06-24 23:42:06 +0900

    perf: enable term-based MSM parallelization

--- zkx/backends/cpu/codegen/cpu_kernel_emitter_bn254_g2_msm_unittest.cc
+++ zkx/backends/cpu/codegen/cpu_kernel_emitter_bn254_g2_msm_unittest.cc
@@ -1,30 +1,26 @@
+#include "xla/tsl/platform/cpu_info.h"
 #include "zkx/backends/cpu/codegen/cpu_kernel_emitter_test.h"
+#include "zkx/base/containers/container_util.h"
 #include "zkx/literal_util.h"
 
 namespace zkx::cpu {
 
 TEST_F(CpuKernelEmitterTest, G2MSM) {
-  const std::string kHloText = R"(
-ENTRY %f (x: bn254.sf[4], y: bn254.g2_affine[4]) -> bn254.g2_jacobian[] {
-  %x = bn254.sf[4] parameter(0)
-  %y = bn254.g2_affine[4] parameter(1)
+  int num_scalar_muls = tsl::port::MaxParallelism() + 1;
+  const std::string kHloText = absl::Substitute(R"(
+ENTRY %f (x: bn254.sf[$0], y: bn254.g2_affine[$0]) -> bn254.g2_jacobian[] {
+  %x = bn254.sf[$0] parameter(0)
+  %y = bn254.g2_affine[$0] parameter(1)
 
   ROOT %ret = bn254.g2_jacobian[] msm(%x, %y)
 }
-)";
+)",
+                                                num_scalar_muls);
 
-  std::vector<math::bn254::Fr> scalars = {
-      math::bn254::Fr::Random(),
-      math::bn254::Fr::Random(),
-      math::bn254::Fr::Random(),
-      math::bn254::Fr::Random(),
-  };
-  std::vector<math::bn254::G2AffinePoint> bases = {
-      math::bn254::G2AffinePoint::Random(),
-      math::bn254::G2AffinePoint::Random(),
-      math::bn254::G2AffinePoint::Random(),
-      math::bn254::G2AffinePoint::Random(),
-  };
+  std::vector<math::bn254::Fr> scalars = base::CreateVector(
+      num_scalar_muls, []() { return math::bn254::Fr::Random(); });
+  std::vector<math::bn254::G2AffinePoint> bases = base::CreateVector(
+      num_scalar_muls, []() { return math::bn254::G2AffinePoint::Random(); });
 
   Literal x_literal = LiteralUtil::CreateR1<math::bn254::Fr>(scalars);
   Literal y_literal = LiteralUtil::CreateR1<math::bn254::G2AffinePoint>(bases);
--- zkx/service/llvm_ir/llvm_util.h
+++ zkx/service/llvm_ir/llvm_util.h
@@ -23,13 +23,16 @@ limitations under the License.
 #include "llvm/IR/Value.h"
 #include "mlir/IR/BuiltinAttributes.h"
 #include "mlir/IR/BuiltinTypes.h"
+#include "mlir/IR/ImplicitLocOpBuilder.h"
 #include "mlir/IR/MLIRContext.h"
 #include "mlir/IR/Operation.h"
 #include "mlir/IR/Types.h"
 #include "mlir/IR/Value.h"
 
+#include "zkir/Dialect/EllipticCurve/IR/EllipticCurveOps.h"
 #include "zkir/Dialect/EllipticCurve/IR/EllipticCurveTypes.h"
 #include "zkir/Dialect/Field/IR/FieldAttributes.h"
+#include "zkir/Dialect/Field/IR/FieldOps.h"
 #include "zkir/Dialect/Field/IR/FieldTypes.h"
 #include "zkir/Dialect/ModArith/IR/ModArithAttributes.h"
 #include "zkir/Dialect/ModArith/IR/ModArithTypes.h"
@@ -225,6 +228,75 @@ mlir::MemRefType ShapeToMLIRMemRefType(const Shape& shape,
 // example, if "shape" is [5 x [10 x i32]], the function returns [5 x 10 x i32].
 mlir::RankedTensorType ShapeToMLIRTensorType(const Shape& shape,
                                              mlir::MLIRContext* context);
+
+template <typename T>
+mlir::Value CreateMLIRPrimeFieldConstant(mlir::ImplicitLocOpBuilder& b,
+                                         const T& value, bool use_montgomery) {
+  if constexpr (T::kUseMontgomery) {
+    if (use_montgomery) {
+      return b.create<mlir::zkir::field::ConstantOp>(
+          GetMLIRPrimeFieldType<T>(b.getContext(), true),
+          ConvertBigIntToAPInt(value.value()));
+    } else {
+      return b.create<mlir::zkir::field::ConstantOp>(
+          GetMLIRPrimeFieldType<T>(b.getContext(), false),
+          ConvertBigIntToAPInt(value.MontReduce()));
+    }
+  } else {
+    DCHECK(!use_montgomery);
+    return b.create<mlir::zkir::field::ConstantOp>(
+        GetMLIRPrimeFieldType<T>(b.getContext(), false),
+        ConvertBigIntToAPInt(value.value()));
+  }
+}
+
+template <typename T>
+mlir::Value CreateMLIRQuadraticExtFieldConstant(mlir::ImplicitLocOpBuilder& b,
+                                                const T& value,
+                                                bool use_montgomery) {
+  return b.create<mlir::zkir::field::ConstantOp>(
+      GetMLIRQuadraticExtFieldType<T>(b.getContext(), use_montgomery),
+      GetMLIRExtQuadraticExtFieldAttr(b.getContext(), value, use_montgomery));
+}
+
+template <typename T>
+mlir::Value CreateMLIRFieldConstant(mlir::ImplicitLocOpBuilder& b,
+                                    const T& value, bool use_montgomery) {
+  if constexpr (T::ExtensionDegree() == 1) {
+    return CreateMLIRPrimeFieldConstant(b, value, use_montgomery);
+  } else {
+    static_assert(T::ExtensionDegree() == 2);
+    return CreateMLIRQuadraticExtFieldConstant(b, value, use_montgomery);
+  }
+}
+
+template <typename T>
+mlir::Value CreateMLIREcPointConstant(mlir::ImplicitLocOpBuilder& b,
+                                      const T& value, bool use_montgomery) {
+  if constexpr (math::IsAffinePoint<T>) {
+    llvm::SmallVector<mlir::Value, 2> values;
+    values.push_back(CreateMLIRFieldConstant(b, value.x(), use_montgomery));
+    values.push_back(CreateMLIRFieldConstant(b, value.y(), use_montgomery));
+    return b.create<mlir::zkir::elliptic_curve::PointOp>(
+        GetMLIRAffinePointType<T>(b.getContext(), use_montgomery), values);
+  } else if constexpr (math::IsJacobianPoint<T>) {
+    llvm::SmallVector<mlir::Value, 3> values;
+    values.push_back(CreateMLIRFieldConstant(b, value.x(), use_montgomery));
+    values.push_back(CreateMLIRFieldConstant(b, value.y(), use_montgomery));
+    values.push_back(CreateMLIRFieldConstant(b, value.z(), use_montgomery));
+    return b.create<mlir::zkir::elliptic_curve::PointOp>(
+        GetMLIRJacobianPointType<T>(b.getContext(), use_montgomery), values);
+  } else {
+    static_assert(math::IsPointXyzz<T>);
+    llvm::SmallVector<mlir::Value, 4> values;
+    values.push_back(CreateMLIRFieldConstant(b, value.x(), use_montgomery));
+    values.push_back(CreateMLIRFieldConstant(b, value.y(), use_montgomery));
+    values.push_back(CreateMLIRFieldConstant(b, value.zz(), use_montgomery));
+    values.push_back(CreateMLIRFieldConstant(b, value.zzz(), use_montgomery));
+    return b.create<mlir::zkir::elliptic_curve::PointOp>(
+        GetMLIRPointXyzzType<T>(b.getContext(), use_montgomery), values);
+  }
+}
 
 }  // namespace zkx::llvm_ir
 
