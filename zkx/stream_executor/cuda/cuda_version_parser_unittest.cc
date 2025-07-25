cf0f9e1ceb5206be609a40b453228c6bb55fc18e
Ryan Kim <chokobole33@gmail.com>
2025-07-25 13:17:00 +0900

    feat(se/cuda): add `ParseCudaVersion()`

---
+++ zkx/stream_executor/cuda/cuda_version_parser_unittest.cc
@@ -0,0 +1,43 @@
+/* Copyright 2024 The OpenXLA Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+==============================================================================*/
+
+#include "zkx/stream_executor/cuda/cuda_version_parser.h"
+
+#include "absl/status/status.h"
+#include "gmock/gmock.h"
+#include "gtest/gtest.h"
+
+#include "xla/tsl/platform/status_matchers.h"
+
+namespace stream_executor {
+namespace {
+using tsl::testing::IsOkAndHolds;
+using tsl::testing::StatusIs;
+
+TEST(CudaVersionParserTest, ValidVersion) {
+  EXPECT_THAT(ParseCudaVersion(12040), IsOkAndHolds(SemanticVersion{12, 4, 0}));
+}
+
+TEST(CudaVersionParserTest, LeastSignificantDigitIsIgnored) {
+  EXPECT_THAT(ParseCudaVersion(12041), IsOkAndHolds(SemanticVersion{12, 4, 0}));
+}
+
+TEST(CudaVersionParserTest, NegativeIntegerIsNotAValidVersion) {
+  EXPECT_THAT(ParseCudaVersion(-42),
+              StatusIs(absl::StatusCode::kInvalidArgument));
+}
+
+}  // namespace
+}  // namespace stream_executor
