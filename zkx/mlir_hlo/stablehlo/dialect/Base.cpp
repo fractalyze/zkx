6ea3e4098f905f1d8a3c7ce43ab008fc661f1a33
Ryan Kim <chokobole33@gmail.com>
2025-09-01 14:14:23 +0900

    feat(mlir_hlo): add helpers for least/most specific type and dim/bound

--- zkx/mlir_hlo/stablehlo/dialect/Base.cpp
+++ zkx/mlir_hlo/stablehlo/dialect/Base.cpp
@@ -194,6 +194,191 @@ Attribute boundsToEncoding(Attribute prototype, ArrayRef<int64_t> bounds) {
   return dialect->createTypeExtensions(bounds);
 }
 
+FailureOr<std::pair<int64_t, int64_t>> inferMostSpecificDimAndBound(
+    std::optional<Location> location, int64_t dim, int64_t leftSize,
+    int64_t rightSize, int64_t leftBound, int64_t rightBound) {
+  bool isLeftStaticDim = isStaticDimSize(leftSize);
+  bool isRightStaticDim = isStaticDimSize(rightSize);
+  bool isLeftStaticBound = isStaticDimSize(leftBound);
+  bool isRightStaticBound = isStaticDimSize(rightBound);
+  int64_t inferredSize = ShapedType::kDynamic;
+  int64_t inferredBound = ShapedType::kDynamic;
+
+  if (isLeftStaticDim || isRightStaticDim) {
+    if (isLeftStaticDim && isRightStaticDim && leftSize != rightSize)
+      return emitOptionalError(location, "Mismatched dimension sizes ",
+                               leftSize, " and ", rightSize, " in dimension ",
+                               dim);
+    inferredSize = isLeftStaticDim ? leftSize : rightSize;
+    if (isLeftStaticBound || isRightStaticBound) {
+      int64_t check_bound = isLeftStaticBound ? leftBound : rightBound;
+      if (inferredSize > check_bound)
+        return emitOptionalError(location, "Mismatched dimension size ",
+                                 inferredSize, " and bound ", check_bound,
+                                 " in dimension ", dim);
+    }
+  } else {
+    if (isLeftStaticBound && isRightStaticBound)
+      inferredBound = std::min(leftBound, rightBound);
+    else
+      inferredBound = isLeftStaticBound ? leftBound : rightBound;
+  }
+  return std::make_pair(inferredSize, inferredBound);
+}
+
+FailureOr<std::pair<int64_t, int64_t>> inferLeastSpecificDimAndBound(
+    std::optional<Location> location, int64_t dim, int64_t leftSize,
+    int64_t rightSize, int64_t leftBound, int64_t rightBound) {
+  bool isLeftStaticDim = isStaticDimSize(leftSize);
+  bool isRightStaticDim = isStaticDimSize(rightSize);
+  bool isLeftStaticBound = isStaticDimSize(leftBound);
+  bool isRightStaticBound = isStaticDimSize(rightBound);
+  int64_t inferredSize = ShapedType::kDynamic;
+  int64_t inferredBound = ShapedType::kDynamic;
+
+  if (isLeftStaticDim || isRightStaticDim) {
+    if (isLeftStaticDim && isRightStaticDim) {
+      if (leftSize != rightSize)
+        return emitOptionalError(location, "Mismatched dimension sizes ",
+                                 leftSize, " and ", rightSize, " in dimension ",
+                                 dim);
+      inferredSize = leftSize;
+    } else if (isLeftStaticBound || isRightStaticBound) {
+      inferredBound = isLeftStaticDim ? std::max(leftSize, rightBound)
+                                      : std::max(rightSize, leftBound);
+    }
+  } else if (isLeftStaticBound && isRightStaticBound) {
+    inferredBound = std::max(leftBound, rightBound);
+  }
+  return std::make_pair(inferredSize, inferredBound);
+}
+
+namespace {
+
+FailureOr<ShapedType> inferTypeWithCustomFn(
+    std::optional<Location> location, SmallVector<RankedTensorType> rankedTypes,
+    std::function<FailureOr<std::pair<int64_t, int64_t>>(
+        std::optional<Location>, int64_t, int64_t, int64_t, int64_t, int64_t)>
+        inferDimAndBoundFn) {
+  auto rank = rankedTypes[0].getRank();
+  for (RankedTensorType type : rankedTypes) {
+    if (type.getRank() != rank) {
+      return emitOptionalError(location, "Mismatched ranks of types",
+                               rankedTypes[0].getRank(), " vs ",
+                               type.getRank());
+    }
+  }
+  SmallVector<int64_t> inferredSizes = to_vector(rankedTypes[0].getShape());
+  SmallVector<int64_t> inferredBounds(rank, ShapedType::kDynamic);
+  ArrayRef<int64_t> bounds = encodingToBounds(rankedTypes[0].getEncoding());
+  if (!bounds.empty()) inferredBounds = to_vector(bounds);
+  bool anyInputHaveBounds = !bounds.empty();
+
+  for (size_t i = 1; i < rankedTypes.size(); ++i) {
+    bounds = encodingToBounds(rankedTypes[i].getEncoding());
+    for (int dim = 0; dim < rank; ++dim) {
+      FailureOr<std::pair<int64_t, int64_t>> inferredDimAndBoundOrErr =
+          inferDimAndBoundFn(location, dim,
+                             /*leftSize=*/inferredSizes[dim],
+                             /*rightSize=*/rankedTypes[i].getShape()[dim],
+                             /*leftBound=*/inferredBounds[dim],
+                             /*rightBound=*/bounds.empty()
+                                 ? ShapedType::kDynamic
+                                 : bounds[dim]);
+      if (failed(inferredDimAndBoundOrErr)) return failure();
+      inferredSizes[dim] = (*inferredDimAndBoundOrErr).first;
+      inferredBounds[dim] = (*inferredDimAndBoundOrErr).second;
+    }
+  }
+
+  return {RankedTensorType::get(
+      inferredSizes, rankedTypes[0].getElementType(),
+      boundsToEncoding(
+          rankedTypes[0].getEncoding(),
+          // Empty array as argument is an indicator to boundsToEncoding() that
+          // there are no bounds at all in inputs, thus sparsity attributes will
+          // be included in the return type
+          anyInputHaveBounds ? inferredBounds : ArrayRef<int64_t>({})))};
+}
+
+FailureOr<Type> inferLeastSpecificShapedType(std::optional<Location> location,
+                                             TypeRange inputTypes) {
+  SmallVector<RankedTensorType> rankedTypes;
+  for (Type inputType : inputTypes)
+    if (auto rankedType = dyn_cast<RankedTensorType>(inputType))
+      rankedTypes.push_back(rankedType);
+    else
+      return inputType;
+  return inferTypeWithCustomFn(location, rankedTypes,
+                               inferLeastSpecificDimAndBound);
+}
+
+FailureOr<Type> inferMostSpecificShapedType(std::optional<Location> location,
+                                            TypeRange inputTypes) {
+  SmallVector<RankedTensorType> rankedTypes;
+  for (Type inputType : inputTypes)
+    if (auto rankedType = dyn_cast<RankedTensorType>(inputType))
+      rankedTypes.push_back(rankedType);
+  if (rankedTypes.empty()) return inputTypes[0];
+  return inferTypeWithCustomFn(location, rankedTypes,
+                               inferMostSpecificDimAndBound);
+}
+
+// Applies `fn` to `inputTypes`, using `location` for errors.
+// If `inputTypes` are tuples, then applies `fn` to them elementwise and
+// wraps the results into a tuple, for example:
+//   mapOverTupleElements({tuple<T11, T12>, tuple<T21, T22>}, fn) =
+//     tuple<fn(T11, T21), fn(T12, T22)>
+// Only supports `inputTypes` where either all types are tuples or no types
+// are tuples.
+FailureOr<Type> mapOverTupleElements(
+    std::optional<Location> location, TypeRange inputTypes,
+    function_ref<FailureOr<Type>(std::optional<Location>, TypeRange types)>
+        fn) {
+  SmallVector<TupleType> tupleTypes;
+  for (Type inputType : inputTypes) {
+    if (auto tupleType = dyn_cast<TupleType>(inputType))
+      tupleTypes.push_back(tupleType);
+  }
+  if (!tupleTypes.empty()) {
+    if (tupleTypes.size() != inputTypes.size())
+      return emitOptionalError(location,
+                               "Mismatched type kinds: either all types ",
+                               "must be tuples, or no types must be tuples");
+    SmallVector<Type> results(tupleTypes[0].size());
+    for (TupleType tupleType : tupleTypes) {
+      if (tupleType.size() != results.size())
+        return emitOptionalError(location,
+                                 "Mismatched tuple sizes: all tuple sizes ",
+                                 "must be the same");
+    }
+    for (size_t i = 0; i < results.size(); ++i) {
+      SmallVector<Type> ithElements;
+      for (TupleType tupleType : tupleTypes)
+        ithElements.push_back(tupleType.getType(i));
+      FailureOr<Type> result = fn(location, ithElements);
+      if (failed(result)) return failure();
+      results[i] = *result;
+    }
+    return TupleType::get(tupleTypes[0].getContext(), results);
+  }
+  return fn(location, inputTypes);
+}
+
+}  // namespace
+
+FailureOr<Type> inferLeastSpecificType(std::optional<Location> location,
+                                       TypeRange inputTypes) {
+  return mapOverTupleElements(location, inputTypes,
+                              inferLeastSpecificShapedType);
+}
+
+FailureOr<Type> inferMostSpecificType(std::optional<Location> location,
+                                      TypeRange inputTypes) {
+  return mapOverTupleElements(location, inputTypes,
+                              inferMostSpecificShapedType);
+}
+
 bool hasSingleBoundedDimension(Type type) {
   RankedTensorType rankedType = dyn_cast<RankedTensorType>(type);
   auto boundedAttr =
