7092ed1ffeddf393f32e7fa34413962f68352a72
Ryan Kim <chokobole33@gmail.com>
2025-09-10 09:42:19 +0900

    feat(codegen): add `EraseDeadFunctionsPass`

---
+++ zkx/codegen/emitters/transforms/erase_dead_functions.cc
@@ -0,0 +1,82 @@
+/* Copyright 2024 The OpenXLA Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+==============================================================================*/
+#include <memory>
+#include <queue>
+
+#include "llvm/ADT/DenseSet.h"
+#include "mlir/Dialect/Func/IR/FuncOps.h"
+#include "mlir/IR/BuiltinOps.h"
+#include "mlir/IR/SymbolTable.h"
+#include "mlir/Interfaces/CallInterfaces.h"
+#include "mlir/Pass/Pass.h"
+#include "mlir/Support/LLVM.h"
+
+namespace zkx::emitters {
+
+#define GEN_PASS_DEF_ERASEDEADFUNCTIONSPASS
+#include "zkx/codegen/emitters/transforms/passes.h.inc"
+
+namespace {
+
+llvm::DenseSet<mlir::func::FuncOp> FindLiveFunctions(mlir::ModuleOp module) {
+  std::queue<mlir::func::FuncOp> worklist;
+  llvm::DenseSet<mlir::func::FuncOp> live_funcs;
+  module.walk([&](mlir::func::FuncOp func) {
+    if (!func.isPrivate()) {
+      worklist.push(func);
+      live_funcs.insert(func);
+    }
+  });
+
+  mlir::SymbolTableCollection symbol_table;
+  while (!worklist.empty()) {
+    mlir::func::FuncOp func = worklist.front();
+    worklist.pop();
+    func.walk([&](mlir::CallOpInterface call) {
+      // NOTE(chokobole): `resolveCallableInTable()` can return nullptr for
+      // indirect calls or unresolved symbols, and the callable might not be a
+      // `mlir::func::FuncOp`.
+      // See https://github.com/zk-rabbit/zkx/pull/77#discussion_r2347138242
+      auto callee = mlir::dyn_cast_or_null<mlir::func::FuncOp>(
+          call.resolveCallableInTable(&symbol_table));
+      if (callee && live_funcs.insert(callee).second) {
+        worklist.push(callee);
+      }
+    });
+  }
+  return live_funcs;
+}
+
+class EraseDeadFunctionsPass
+    : public impl::EraseDeadFunctionsPassBase<EraseDeadFunctionsPass> {
+ public:
+  void runOnOperation() override {
+    // Find live functions and erase dead ones.
+    auto live = FindLiveFunctions(getOperation());
+    getOperation().walk([&](mlir::func::FuncOp func) {
+      if (!live.contains(func)) {
+        func.erase();
+      }
+    });
+  }
+};
+
+}  // namespace
+
+std::unique_ptr<mlir::Pass> CreateEraseDeadFunctionsPass() {
+  return std::make_unique<EraseDeadFunctionsPass>();
+}
+
+}  // namespace zkx::emitters
