8097b424b4f397abc7292a5b4a41e9e51acad527
Ryan Kim <chokobole33@gmail.com>
2025-09-18 14:31:33 +0900

    feat(mlir_hlo): add `ConvertToSignlessPass`

---
+++ zkx/mlir_hlo/mhlo/transforms/passes.h
@@ -0,0 +1,37 @@
+/* Copyright 2019 The OpenXLA Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+==============================================================================*/
+
+#ifndef ZKX_MLIR_HLO_MHLO_TRANSFORMS_PASSES_H_
+#define ZKX_MLIR_HLO_MHLO_TRANSFORMS_PASSES_H_
+
+#include <memory>
+
+#include "mlir/IR/BuiltinOps.h"
+#include "mlir/Pass/Pass.h"
+
+namespace mlir::mhlo {
+
+#define GEN_PASS_DECL
+#include "zkx/mlir_hlo/mhlo/transforms/mhlo_passes.h.inc"
+
+// Pass to replace unsigned types with signless integers.
+std::unique_ptr<OperationPass<ModuleOp>> createConvertToSignlessPass();
+
+#define GEN_PASS_REGISTRATION
+#include "zkx/mlir_hlo/mhlo/transforms/mhlo_passes.h.inc"  // NOLINT(build/include)
+
+}  // namespace mlir::mhlo
+
+#endif  // ZKX_MLIR_HLO_MHLO_TRANSFORMS_PASSES_H_
