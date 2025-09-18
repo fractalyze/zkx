2468bfcbd293a37164ba4335f3682711dc0a164c
Ryan Kim <chokobole33@gmail.com>
2025-09-18 15:48:25 +0900

    feat(service/gpu): add `TestGpuDeviceInfo`

---
+++ zkx/service/gpu/gpu_device_info_for_tests.h
@@ -0,0 +1,34 @@
+/* Copyright 2022 The OpenXLA Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+==============================================================================*/
+
+#ifndef ZKX_SERVICE_GPU_GPU_DEVICE_INFO_FOR_TESTS_H_
+#define ZKX_SERVICE_GPU_GPU_DEVICE_INFO_FOR_TESTS_H_
+
+#include "zkx/stream_executor/device_description.h"
+
+namespace zkx::gpu {
+
+class TestGpuDeviceInfo {
+ public:
+  static se::DeviceDescription RTXA6000DeviceInfo(
+      se::GpuComputeCapability cc = se::CudaComputeCapability(8, 9));
+  static se::DeviceDescription AMDMI210DeviceInfo();
+  // Returns default RTXA6000 or AMDMI210 device info
+  static se::DeviceDescription CudaOrRocmDeviceInfo();
+};
+
+}  // namespace zkx::gpu
+
+#endif  // ZKX_SERVICE_GPU_GPU_DEVICE_INFO_FOR_TESTS_H_
