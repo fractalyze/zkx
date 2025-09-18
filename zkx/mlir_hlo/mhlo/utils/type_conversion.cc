35e846aca437d6575d53cf41de3bccab6921a988
Ryan Kim <chokobole33@gmail.com>
2025-09-18 13:42:02 +0900

    feat(mlir_hlo): add `RemoveSignTypeConverter`

---
+++ zkx/mlir_hlo/mhlo/utils/type_conversion.cc
@@ -0,0 +1,76 @@
+/* Copyright 2021 The OpenXLA Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+==============================================================================*/
+
+#include "zkx/mlir_hlo/mhlo/utils/type_conversion.h"
+
+#include "mlir/IR/Builders.h"
+#include "mlir/IR/BuiltinOps.h"
+#include "mlir/IR/BuiltinTypes.h"
+#include "mlir/IR/TypeUtilities.h"
+#include "mlir/IR/Types.h"
+#include "mlir/IR/Value.h"
+#include "mlir/Support/LLVM.h"
+
+namespace mlir::mhlo {
+
+namespace {
+
+Type convertInteger(IntegerType intType) {
+  return IntegerType::get(intType.getContext(), intType.getWidth());
+}
+
+Type convertShapedType(ShapedType shapedType) {
+  if (auto intType = dyn_cast<IntegerType>(shapedType.getElementType()))
+    return shapedType.clone(convertInteger(intType));
+  return shapedType;
+}
+
+Value materializeCastFromIllegal(OpBuilder &builder, Type type,
+                                 ValueRange inputs, Location loc) {
+  Type fromType = getElementTypeOrSelf(inputs[0].getType());
+  Type toType = getElementTypeOrSelf(type);
+  if ((!fromType.isSignedInteger() && !fromType.isUnsignedInteger()) ||
+      !toType.isSignlessInteger())
+    return Value();
+  // Use unrealized conversion casts to do signful->signless conversions.
+  return builder.create<UnrealizedConversionCastOp>(loc, type, inputs[0])
+      ->getResult(0);
+}
+
+Value materializeCastToIllegal(OpBuilder &builder, Type type, ValueRange inputs,
+                               Location loc) {
+  Type fromType = getElementTypeOrSelf(inputs[0].getType());
+  Type toType = getElementTypeOrSelf(type);
+  if (!fromType.isSignlessInteger() ||
+      (!toType.isSignedInteger() && !toType.isUnsignedInteger()))
+    return Value();
+  // Use unrealized conversion casts to do signless->signful conversions.
+  return builder.create<UnrealizedConversionCastOp>(loc, type, inputs[0])
+      ->getResult(0);
+}
+
+}  // namespace
+
+RemoveSignTypeConverter::RemoveSignTypeConverter() {
+  addConversion([](Type type) { return type; });
+
+  addConversion(convertInteger);
+  addConversion(convertShapedType);
+
+  addSourceMaterialization(materializeCastToIllegal);
+  addTargetMaterialization(materializeCastFromIllegal);
+}
+
+}  // namespace mlir::mhlo
---
+++ zkx/mlir_hlo/mhlo/utils/type_conversion.h
@@ -0,0 +1,32 @@
+/* Copyright 2021 The OpenXLA Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+==============================================================================*/
+
+#ifndef ZKX_MLIR_HLO_MHLO_UTILS_TYPE_CONVERSION_H_
+#define ZKX_MLIR_HLO_MHLO_UTILS_TYPE_CONVERSION_H_
+
+#include "mlir/Transforms/DialectConversion.h"
+
+namespace mlir::mhlo {
+
+// Type converter to use as part of lowerings from dialects that carry signs
+// in their types to those that are signless.
+class RemoveSignTypeConverter : public TypeConverter {
+ public:
+  RemoveSignTypeConverter();
+};
+
+}  // namespace mlir::mhlo
+
+#endif  // ZKX_MLIR_HLO_MHLO_UTILS_TYPE_CONVERSION_H_
