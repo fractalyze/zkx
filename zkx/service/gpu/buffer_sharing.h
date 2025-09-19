7e5ca5cd54f41933fc87285feeedd92f02a9d13c
Ryan Kim <chokobole33@gmail.com>
2025-09-19 12:53:54 +0900

    feat(service/gpu): add `(Fusion)CanShareBufferHint()`

---
+++ zkx/service/gpu/buffer_sharing.h
@@ -0,0 +1,39 @@
+/* Copyright 2023 The OpenXLA Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+==============================================================================*/
+
+#ifndef ZKX_SERVICE_GPU_BUFFER_SHARING_H_
+#define ZKX_SERVICE_GPU_BUFFER_SHARING_H_
+
+#include <optional>
+
+#include "zkx/hlo/ir/hlo_instruction.h"
+#include "zkx/shape_util.h"
+#include "zkx/stream_executor/device_description.h"
+
+namespace zkx::gpu {
+
+std::optional<bool> FusionCanShareBufferHint(
+    const HloInstruction* user, const HloInstruction* operand,
+    const ShapeIndex& user_index,
+    const se::DeviceDescription& device_description);
+
+std::optional<bool> CanShareBufferHint(
+    const HloInstruction* user, const HloInstruction* operand,
+    const ShapeIndex& user_index,
+    const se::DeviceDescription& device_description);
+
+}  // namespace zkx::gpu
+
+#endif  // ZKX_SERVICE_GPU_BUFFER_SHARING_H_
