d3da8d82d47399d43b443961d2a61c53f1ec6cf4
Ryan Kim <chokobole33@gmail.com>
2025-09-25 21:59:20 +0900

    feat(service/gpu): add `CantFindCudaMessage()`

---
+++ zkx/service/gpu/llvm_gpu_backend/nvptx_utils_unittest.cc
@@ -0,0 +1,25 @@
+/* Copyright 2024 The OpenXLA Authors.
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+    http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+==============================================================================*/
+#include "zkx/service/gpu/llvm_gpu_backend/nvptx_utils.h"
+
+#include "gtest/gtest.h"
+
+namespace zkx::gpu::nvptx {
+namespace {
+
+TEST(NvptxUtilsTest, CantFindCudaMessageTest) {
+  std::string msg = CantFindCudaMessage("foo", "/bar");
+  EXPECT_NE(msg.length(), 0);
+}
+
+}  // namespace
+}  // namespace zkx::gpu::nvptx
