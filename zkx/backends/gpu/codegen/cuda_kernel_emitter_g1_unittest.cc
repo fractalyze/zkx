46f520c031038a97f1f7cacedbd071171c7862e9
Ryan Kim <chokobole33@gmail.com>
2025-10-02 07:41:03 +0900

    test(backends/gpu): add cuda kernel emitter test for group

---
+++ zkx/backends/gpu/codegen/cuda_kernel_emitter_g1_unittest.cc
@@ -0,0 +1,61 @@
+#include "zkx/backends/gpu/codegen/group_test.h"
+
+namespace zkx::gpu {
+
+using GroupTypes = testing::Types<math::bn254::G1AffinePoint>;
+TYPED_TEST_SUITE(GroupScalarUnaryTest, GroupTypes);
+
+TYPED_TEST(GroupScalarUnaryTest, Convert) {
+  this->SetUpConvert();
+  this->RunAndVerify();
+}
+
+TYPED_TEST(GroupScalarUnaryTest, Negate) {
+  this->SetUpNegate();
+  this->RunAndVerify();
+}
+
+TYPED_TEST_SUITE(GroupScalarBinaryTest, GroupTypes);
+
+TYPED_TEST(GroupScalarBinaryTest, Add) {
+  this->SetUpAdd();
+  this->RunAndVerify();
+}
+
+TYPED_TEST(GroupScalarBinaryTest, Double) {
+  this->SetUpDouble();
+  this->RunAndVerify();
+}
+
+TYPED_TEST(GroupScalarBinaryTest, Sub) {
+  this->SetUpSub();
+  this->RunAndVerify();
+}
+
+TYPED_TEST(GroupScalarBinaryTest, ScalarMul) {
+  this->SetUpScalarMul();
+  this->RunAndVerify();
+}
+
+TYPED_TEST_SUITE(GroupR2TensorUnaryTest, GroupTypes);
+
+TYPED_TEST(GroupR2TensorUnaryTest, Negate) {
+  this->SetUpNegate();
+  this->RunAndVerify();
+}
+
+TYPED_TEST_SUITE(GroupR2TensorBinaryTest, GroupTypes);
+
+TYPED_TEST(GroupR2TensorBinaryTest, Add) {
+  this->SetUpAdd();
+  this->RunAndVerify();
+}
+
+TYPED_TEST_SUITE(GroupTest, GroupTypes);
+
+TYPED_TEST(GroupTest, Slice) {
+  this->SetUpSlice();
+  this->RunAndVerify();
+}
+
+}  // namespace zkx::gpu
