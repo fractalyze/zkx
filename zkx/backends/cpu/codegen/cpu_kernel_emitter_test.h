4890efa4e12c504f94b8da0a4821933028771652
Ryan Kim <chokobole33@gmail.com>
2025-10-02 07:36:56 +0900

    refactor: separate platform-independent code `CpuKernelEmitterTest`

--- zkx/backends/cpu/codegen/cpu_kernel_emitter_test.h
+++ zkx/backends/cpu/codegen/cpu_kernel_emitter_test.h
@@ -1,32 +1,13 @@
 #ifndef ZKX_BACKENDS_CPU_CODEGEN_CPU_KERNEL_EMITTER_TEST_H_
 #define ZKX_BACKENDS_CPU_CODEGEN_CPU_KERNEL_EMITTER_TEST_H_
 
-#include <string>
-#include <string_view>
-#include <vector>
-
-#include "gtest/gtest.h"
-
-#include "zkx/literal.h"
-#include "zkx/service/hlo_runner.h"
+#include "zkx/backends/kernel_emitter_test.h"
 
 namespace zkx::cpu {
 
-class CpuKernelEmitterTest : public testing::Test {
+class CpuKernelEmitterTest : public KernelEmitterTest {
  public:
-  CpuKernelEmitterTest();
-
-  void RunAndVerify();
-
- protected:
-  virtual void Verify(const Literal& ret_literal) const;
-
-  HloRunner runner_;
-  std::string_view x_typename_;
-  std::vector<Literal> literals_;
-  std::string hlo_text_;
-  Literal expected_literal_;
-  absl::StatusCode expected_status_code_ = absl::StatusCode::kOk;
+  CpuKernelEmitterTest() : KernelEmitterTest("cpu") {}
 };
 
 }  // namespace zkx::cpu
