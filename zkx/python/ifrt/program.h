55fb963bdee450042abad15b7ea55c668cc345ef
Ryan Kim <chokobole33@gmail.com>
2025-10-17 10:11:31 +0900

    feat(python): add `Program`

---
+++ zkx/python/ifrt/program.h
@@ -0,0 +1,34 @@
+/* Copyright 2024 The OpenXLA Authors.
+Copyright 2025 The ZKX Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+==============================================================================*/
+
+#ifndef ZKX_PYTHON_IFRT_PROGRAM_H_
+#define ZKX_PYTHON_IFRT_PROGRAM_H_
+
+#include "llvm/Support/ExtensibleRTTI.h"
+
+#include "zkx/python/ifrt/serdes.h"
+
+namespace zkx::ifrt {
+
+// Abstract program. A program is any serializable code that can be compiled and
+// loaded onto devices to create a `LoadedExecutable`.
+struct Program : llvm::RTTIExtends<Program, Serializable> {
+  static char ID;
+};
+
+}  // namespace zkx::ifrt
+
+#endif  // ZKX_PYTHON_IFRT_PROGRAM_H_
