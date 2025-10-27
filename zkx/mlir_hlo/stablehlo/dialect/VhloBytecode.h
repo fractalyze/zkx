85bfe0b2567fb3d936160bc274f691eb42b75028
Ryan Kim <chokobole33@gmail.com>
2025-10-28 07:25:21 +0900

    feat(mlir_hlo): add vhlo impls

---
+++ zkx/mlir_hlo/stablehlo/dialect/VhloBytecode.h
@@ -0,0 +1,30 @@
+/* Copyright 2022 The StableHLO Authors.
+Copyright 2025 The ZKX Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+==============================================================================*/
+
+#ifndef ZKX_MLIR_HLO_STABLEHLO_DIALECT_VHLOBYTECODE_H_
+#define ZKX_MLIR_HLO_STABLEHLO_DIALECT_VHLOBYTECODE_H_
+
+namespace mlir::vhlo {
+
+class VhloDialect;
+
+// Add the interface necessary for encoding and decoding VHLO dialect
+// components in bytecode.
+void addBytecodeInterface(VhloDialect *dialect);
+
+} // namespace mlir::vhlo
+
+#endif // ZKX_MLIR_HLO_STABLEHLO_DIALECT_VHLOBYTECODE_H_
