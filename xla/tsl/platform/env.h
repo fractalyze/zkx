28be458662c02de7bd7d3eca0ca2822cf3302b86
Ryan Kim <chokobole33@gmail.com>
2025-11-18 16:29:09 +0900

    chore(tsl): remove outdated comments

--- xla/tsl/platform/env.h
+++ xla/tsl/platform/env.h
@@ -40,10 +40,6 @@ namespace tsl {
 class Thread;
 struct ThreadOptions;
 
-// NOTE(chokobole): This class includes portions of the Env class to enable
-// zkx::thread::ThreadPool and zkx::cpu::ThunkExecutor from:
-// https://github.com/openxla/xla/blob/8bac4a2/xla/tsl/platform/env.h.
-// It is planned to be replaced with Chromium's implementation in the future.
 class Env {
  public:
   Env();
@@ -466,8 +462,6 @@ class Env {
   void operator=(const Env&) = delete;
 };
 
-// NOTE(chokobole): It is planned to be replaced with Chromium's implementation
-// in the future.
 /// Represents a thread used to run a TSL function.
 class Thread {
  public:
@@ -490,8 +484,6 @@ int setenv(const char* name, const char* value, int overwrite);
 /// Cross-platform unsetenv.
 int unsetenv(const char* name);
 
-// NOTE(chokobole): It is planned to be replaced with Chromium's implementation
-// in the future.
 /// \brief Options to configure a Thread.
 ///
 /// Note that the options are all hints, and the
