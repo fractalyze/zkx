b52e1329a7eb0e2e00378708d0e78fd1a71ead08
Ryan Kim <chokobole33@gmail.com>
2026-01-11 20:56:26 +0900

    feat(service): add `CallInliner` pass for HLO call inlining
    Introduces the `CallInliner` pass to optimize HLO modules by inlining
    subcomputations from `kCall` instructions into their callers.

---
+++ zkx/service/call_inliner.h
@@ -0,0 +1,70 @@
+/* Copyright 2017 The OpenXLA Authors.
+Copyright 2026 The ZKX Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+==============================================================================*/
+
+#ifndef ZKX_SERVICE_CALL_INLINER_H_
+#define ZKX_SERVICE_CALL_INLINER_H_
+
+#include <string>
+#include <utility>
+
+#include "absl/container/flat_hash_map.h"
+
+#include "zkx/hlo/pass/hlo_pass_interface.h"
+
+namespace zkx {
+
+// For every kCall operation in the main computation, we inline the body of the
+// called function, and proceed recursively.
+class CallInliner : public HloModulePass {
+ public:
+  using InlinedInstructionMap =
+      absl::flat_hash_map<HloInstruction*, HloInstruction*>;
+
+  // Inlines one call instruction. Returns a mapping from the original
+  // instructions to their inlined versions.
+  static absl::StatusOr<InlinedInstructionMap> Inline(HloInstruction* call);
+
+  // If single_call_site is true, only functions with a single call site will be
+  // inlined.
+  // If update_domain is true, the exit domains could be updated for calls which
+  // are being inlined if necessary.
+  explicit CallInliner(
+      bool single_call_site = false, bool update_domain = false,
+      absl::flat_hash_set<std::string> composites_to_preserve = {})
+      : single_call_site_(single_call_site),
+        update_domain_(update_domain),
+        composites_to_preserve_(std::move(composites_to_preserve)) {}
+  ~CallInliner() override = default;
+  std::string_view name() const override { return "call-inliner"; }
+
+  using HloPassInterface::Run;
+  absl::StatusOr<bool> Run(
+      HloModule* module,
+      const absl::flat_hash_set<std::string_view>& execution_threads) override;
+
+  // Returns true if the instruction is a kCall operation and is eligible for
+  // inlining.
+  virtual bool IsInlineableCallOp(HloInstruction* instruction) const;
+
+ private:
+  bool single_call_site_;
+  bool update_domain_;
+  absl::flat_hash_set<std::string> composites_to_preserve_;
+};
+
+}  // namespace zkx
+
+#endif  // ZKX_SERVICE_CALL_INLINER_H_
